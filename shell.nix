d0dd073455f9e86ba401539a3ffc72ee2f116dff
Jesse Wattenbarger <jwatt@broken.watch>
2020-02-20 23:41:18 -0500

    Add shell.nix for building on nixos.

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} };
+pkgs.mkShell {
+	buildInputs = with pkgs; [
+		gcc
+		glib
+		pkgconfig
+		autoconf
+		automake
+		libtool
+		pcre
+		gnumake
+		qt5.full
+		qt5.qmake
+		zlib
+	];
+}
+
